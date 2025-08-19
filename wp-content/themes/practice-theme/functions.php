459a2a80a9bc00e863fc0eed3c0c9645b168fb6e
OfriG <96420730+OfriG@users.noreply.github.com>
2025-08-19 15:04:25 +0300

    navbar pr fixes

--- wp-content/themes/practice-theme/functions.php
+++ wp-content/themes/practice-theme/functions.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once get_template_directory() . '/Functions/theme-setupFunctions.php';
+require_once get_template_directory() . '/Functions/themeSetupFunctions.php';
 require_once get_template_directory() . '/Functions/loadAssestsFunctions.php';
 require_once get_template_directory() . '/Functions/logoFunctions.php';
-require_once get_template_directory() . '/Functions/addScssToMenuFunc.php';
