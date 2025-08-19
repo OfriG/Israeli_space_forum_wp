c34354c17683c4e17fbcf4fd3c559ff493d57ab7
OfriG <ofri900@gmail.com>
2025-08-19 18:34:23 +0300

    just add html file

--- wp-content/themes/practice-theme/front-page.php
+++ wp-content/themes/practice-theme/front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Front page template
  * This file displays the homepage content
@@ -8,8 +9,7 @@ get_header(); ?>
 
 <main id="main" class="site-main">
     <div class="container">
-        <h1>עפרי המלכה</h1>
-        <p>Welcome to our homepage!</p>
+
     </div>
 </main>
 
