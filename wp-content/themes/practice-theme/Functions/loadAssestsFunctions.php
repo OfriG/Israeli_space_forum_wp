75f746d34b88c5bf96708755357d2f765bf40f24
OfriG <ofri900@gmail.com>
2025-08-24 19:11:32 +0300

    fix footer

--- wp-content/themes/practice-theme/Functions/loadAssestsFunctions.php
+++ wp-content/themes/practice-theme/Functions/loadAssestsFunctions.php
@@ -12,6 +12,8 @@ function theme_enqueue_assets()
     wp_enqueue_style('navbar-styles', get_template_directory_uri() . '/dist/css/_navbar-components.css', array(), '1.0.0');
     wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/css/_main-styles.css', array(), '1.0.0');
     wp_enqueue_style('footer-styles', get_template_directory_uri() . '/dist/css/_footer.css', array(), '1.0.0');
+    wp_enqueue_style('upper-footer-styles', get_template_directory_uri() . '/dist/css/footer/_upper-footer.css', array(), '1.0.0');
+    wp_enqueue_style('bottom-footer-styles', get_template_directory_uri() . '/dist/css/footer/_bottom-footer.css', array(), '1.0.1');
 
     // JavaScript
     wp_enqueue_script('theme-js', get_template_directory_uri() . '/dist/js/app.js', array('jquery'), '1.0.0', true);
