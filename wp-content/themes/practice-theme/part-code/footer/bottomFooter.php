f077591c0ff73bb531af0e443d24da2dd4344bca
OfriG <ofri900@gmail.com>
2025-08-24 20:24:12 +0300

    fix footer

---
+++ wp-content/themes/practice-theme/part-code/footer/bottomFooter.php
@@ -0,0 +1,41 @@
+<div class="mobile-bottom-section">
+    <div class="menus">
+        <div class="footer-lower-nav">
+            <nav aria-label="<?php echo esc_attr__('Footer menu', 'practice-theme'); ?>">
+                <?php
+                wp_nav_menu([
+                    'theme_location' => 'secondary',
+                    'container'      => false,
+                    'menu_class'     => 'menu-lfooter-lower',
+                    'depth'          => 1,
+                ]);
+                ?>
+            </nav>
+        </div>
+
+        <div class="footer-terms">
+            <p>Terms of use</p>
+            <p>Accessibility</p>
+            <p>Privacy Policy</p>
+        </div>
+    </div>
+    
+    <div class="footer-thanks">
+        <p class="credits-line">
+            Designed and developed by
+            <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/Moveo.png')); ?>" alt="Moveo" />
+        </p>
+        <p>*All space images were taken by Eytan Stibbe </p>
+        <p>during Rakia AX1 Mission.</p>
+    </div>
+    
+    <div class="logos">
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/startup-nation-central.png')); ?>" />
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/israeli-high-teck.png')); ?>" />
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/israel-export.png')); ?>" />
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/ISA.png')); ?>" />
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/ministry.png')); ?>" />
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/innovation.png')); ?>" />
+    </div>
+</div>
+
---
+++ wp-content/themes/practice-theme/part-code/footer/upFooter.php
@@ -0,0 +1,25 @@
+<!-- Mobile upper footer -->
+<div class="mobile-section">
+<div class="logo-powered">
+    <?php display_site_logo(); ?>
+    <div class="powered-by">
+        <p>Powered by</p>
+        <img src="<?php echo esc_url(get_theme_file_uri('images/footer-icons/RAKIA.png')); ?>" alt="RAKIA Logo" />
+    </div>
+</div>
+
+<div class="newsletter-container">
+    <h2 class="follow-news">FOLLOW OUR<br>NEWSLETTER</h2>
+    <p class="subscribe">Subscribe to the ISF newsletter to receive the latest announcements, opportunities, event invitations and more.</p>
+    <input type="email" placeholder="Email" class="email-input"/>
+
+    <div class="newsletter-actions">
+        <button class="subscribe-btn">Subscribe</button>
+        <a href="#" class="linkedin-link">
+            <span class="linkedin-icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/LinkedIn.svg" alt="LinkedIn" />
+            </span>
+        </a>
+    </div>
+</div>
+<div class="line"></div></div>
