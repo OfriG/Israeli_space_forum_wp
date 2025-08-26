0ae2b280335b29bb373e79038e8f5365f93130fb
OfriG <ofri900@gmail.com>
2025-08-26 11:47:29 +0300

    fix pr

--- wp-content/themes/practice-theme/template-parts/footer/newsLetterFooter.php
+++ wp-content/themes/practice-theme/template-parts/footer/newsLetterFooter.php
@@ -1,25 +1,37 @@
   <h2 class="follow-news">
       <?php
-        $title = get_field('newsletter_title', 17);
+        $front_page_id = get_option('page_on_front');
+
+
+        $title = get_field('newsletter_title', $front_page_id);
         if ($title) {
-            echo wp_kses_post($title);
+            // Use nl2br only on mobile devices- not work on localhost only on mobile devices 
+            //its for the case that the title has line breaks
+            if (wp_is_mobile()) {
+                echo wp_kses_post(nl2br($title));
+            } else {
+                // Remove line breaks for desktop to prevent wrapping
+                echo wp_kses_post(str_replace(["\n", "\r"], ' ', $title));
+            }
         } else {
-            echo 'fsgvr';
+            echo 'ewfwe';
         }
+
+
         ?>
   </h2>
   <p class="subscribe">
       <?php
-        $description = get_field('newsletter_description', 17);
+        $description = get_field('newsletter_description', $front_page_id);
 
         if ($description) {
-            echo wp_kses_post($description);
+            echo wp_kses_post(nl2br($description));
         } else {
             echo 'usgdew';
         }
         ?>
   </p>
-  <form class="newsletter-form-mobile" method="post" action="">
+  <form class="newsletter-form" method="post" action="">
       <?php wp_nonce_field('newsletter_subscribe', 'newsletter_nonce'); ?>
       <input type="email" name="subscriber_email" placeholder="Email" class="email-input" required />
 
