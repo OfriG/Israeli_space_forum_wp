15ec3f01d47f02e7dbecb1f6eabe91ee1b2b8a45
OfriG <ofri900@gmail.com>
2025-08-26 13:24:06 +0300

    no message

---
+++ wp-content/themes/practice-theme/hp.php
@@ -0,0 +1,28 @@
+//partcode 1
+<div class="shape-future-section">
+    <div class="shape-future-content">
+        <h1>shaping the future of space.</h1>
+        <h1>together</h1>
+        <div class="shape-future-text">
+            <p>Leading Israel’s dynamic and fast-growing space industry, the Israel Space Forum is dedicated to providing opportunities and fostering local and global collaborations, with a mission to advance Israel’s space capabilities.</p>
+        </div>
+        <div class="joinud-btn">JOIN US</div>
+    </div>
+</div>
+
+//partcode 2
+<div class="number-section">
+    <div class=item1>
+        <h1>63M$</h1>
+        <p>
+            Raised in 2024 by Israeli space-related startups
+        </p>
+    </div>
+    <div class=item2>
+        <h1>60</h1>
+        <p>
+            space companies and startups
+        </p>
+
+
+    </div>
