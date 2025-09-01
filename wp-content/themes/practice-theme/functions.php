2e0bb7f1ecff8c302aefb46821f86ff5532be475
OfriG <ofri900@gmail.com>
2025-09-01 11:07:29 +0300

    no message

--- wp-content/themes/practice-theme/functions.php
+++ wp-content/themes/practice-theme/functions.php
@@ -7,5 +7,4 @@ require_once get_template_directory() . '/Functions/themeSetupFunctions.php';
 require_once get_template_directory() . '/Functions/loadAssestsFunctions.php';
 require_once get_template_directory() . '/Functions/logoFunctions.php';
 require_once get_template_directory() . '/Functions/newsLetterFunctions.php';
-require_once get_template_directory() . '/Functions/homePageFunctions.php';
-require_once get_template_directory() . '/Functions/numberBannerFunctions.php';
+require_once get_template_directory() . '/Functions/registerAcfBlocks.php';
