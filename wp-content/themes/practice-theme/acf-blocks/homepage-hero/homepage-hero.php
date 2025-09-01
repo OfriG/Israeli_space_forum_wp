9360bf9fe44560b5ae09361b355d2c8c2aec2c4f
OfriG <ofri900@gmail.com>
2025-09-01 16:00:34 +0300

    fix pr

--- wp-content/themes/practice-theme/acf-blocks/homepage-hero/homepage-hero.php
+++ wp-content/themes/practice-theme/acf-blocks/homepage-hero/homepage-hero.php
@@ -1,10 +1,6 @@
 <?php
 
-// Create unique id
-$id = 'homepage-hero-' . $block['id'];
-if (!empty($block['anchor'])) {
-    $id = $block['anchor'];
-}
+
 
 // Create class attribute allowing for custom "className"
 $className = 'homepage-hero';
@@ -18,18 +14,18 @@ $description  = get_field('description');
 $button   = get_field('button_text');
 $highlight_headline = get_field('highlight_headline');
 
-$inline_style = '';
 if (!empty($background_image_field)) {
-    // If the field returns an array (image object), get the URL
-    $image_url = is_array($background_image_field) ? $background_image_field['url'] : $background_image_field;
-    $inline_style = "style=\"background-image: url('" . esc_url($image_url) . "');\"";
+    $background_image_field = is_array($background_image_field) ? $background_image_field['url'] : $background_image_field;
 }
 ?>
 
-<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> hero" <?php echo $inline_style; ?>>
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?> homepage-hero-block">
+    <?php if (!empty($background_image_field)) : ?>
+        <div class="hero-background" style="background-image: url('<?php echo esc_url($background_image_field); ?>')"></div>
+    <?php endif; ?>
 
 
-    <div class="shape-future-content">
+    <div class="content-wrapper">
         <h1>
             <?php echo esc_html($headline); ?>
             <?php if (!empty($highlight_headline)) : ?>
@@ -41,7 +37,11 @@ if (!empty($background_image_field)) {
         <p ><?php echo esc_html($description); ?></p>
 
         <?php if (!empty($button)) : ?>
-            <a class="joinud-btn" href="#"><?php echo esc_html($button); ?></a>
+            <?php if (is_array($button)) : ?>
+                <a class="joinud-btn" href="<?php echo esc_url($button['url']); ?>" target="<?php echo esc_attr($button['target'] ?: '_self'); ?>"><?php echo esc_html($button['title']); ?></a>
+            <?php else : ?>
+                <a class="joinud-btn" href="#"><?php echo esc_html($button); ?></a>
+            <?php endif; ?>
         <?php endif; ?>
     </div>
 </div>
