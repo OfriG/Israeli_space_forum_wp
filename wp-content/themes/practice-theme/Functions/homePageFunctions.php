561b4ae3f063e66b5f4e30ba38f75511c3549d29
OfriG <ofri900@gmail.com>
2025-09-01 09:26:56 +0300

    .

--- wp-content/themes/practice-theme/Functions/homePageFunctions.php
+++ wp-content/themes/practice-theme/Functions/homePageFunctions.php
@@ -11,26 +11,10 @@ function practice_theme_register_acf_blocks()
 
         'description' => ('A custom hero block with a headline, description, and a button.'),
         'category' => 'theme',
-    'icon' => 'superhero-alt',
+        'icon' => 'superhero-alt',
         'keywords' => array('hero', 'header', 'banner'),
         'mode' => 'preview',
         'render_template' => get_template_directory() . '/template-parts/home-page/hero.php',
-        'supports' => array(
-            'align' => false,
-            'anchor' => true
-        )
-    ));
-
-    acf_register_block_type(array(
-        'name' => 'numberBanner',
-        'title' => 'Number Banner',
-
-        'description' => ('A custom number banner block with information about the number of startups, companies, and money raised.'),
-        'category' => 'theme',
-    'icon' => 'dashicons-welcome-learn-more',
-        'keywords' => array('number', 'banner'),
-        'mode' => 'preview',
-        'render_template' => get_template_directory() . '/template-parts/home-page/numberBanner.php',
         'supports' => array(
             'align' => false,
             'anchor' => true
