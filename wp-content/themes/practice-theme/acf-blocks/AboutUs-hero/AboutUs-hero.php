b3b92021842e84bbaa6ad02a8b444cf82addffa4
OfriG <ofri900@gmail.com>
2025-09-08 11:51:25 +0300

    about us hero block

--- wp-content/themes/practice-theme/acf-blocks/AboutUs-hero/AboutUs-hero.php
+++ wp-content/themes/practice-theme/acf-blocks/AboutUs-hero/AboutUs-hero.php
@@ -4,12 +4,9 @@ $description = get_field('description');
 $button = get_field('button');
 $background_image = get_field('background_image');
 ?>
-<?php
-if (!empty($background_image)) {
-    $background_image = is_array($background_image) ? $background_image['url'] : $background_image;
-}
-?>
-<div class="AboutUs-hero" style="background-image: url('<?php echo esc_url($background_image); ?>');">
+<div class="AboutUs-hero" <?php if ($background_image && $background_image['url']): ?>
+    style="background-image: url('<?php echo esc_url($background_image['url']); ?>');"
+    <?php else: ?>style="background-color: $gray; height: pxToRem(50);"<?php endif; ?>>
     <div class="AboutUs-hero-content">
         <h1><?php echo nl2br(esc_html($headline)); ?></h1>
         <p><?php echo nl2br(esc_html($description)); ?></p>
