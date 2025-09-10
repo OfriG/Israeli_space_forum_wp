80495497c80bd2a671af9cc7666baef189bff926
OfriG <ofri900@gmail.com>
2025-09-10 11:59:10 +0300

    mobile version

--- wp-content/themes/practice-theme/acf-blocks/impact-block/impact-block.php
+++ wp-content/themes/practice-theme/acf-blocks/impact-block/impact-block.php
@@ -1,25 +1,45 @@
 <?php 
 $impact_block_headline= get_field('headline');
 $impact_block_description= get_field('description');
-$impact_block_button= get_field('button');
+$button= get_field('button');
 $impact_block_background_image= get_field('background_image');
 $video= get_field('video');
 ?>
-
-<div class="impact-block" <?php if ($impact_block_background_image && $impact_block_background_image['url']): ?>
+//mobile
+<div class="impact-block-mobile" <?php if ($impact_block_background_image && $impact_block_background_image['url']): ?>
     style="background-image: url('<?php echo esc_url($impact_block_background_image['url']); ?>');"
     <?php else: ?>style="background-color: $gray; height: pxToRem(50);"<?php endif; ?>>
-    <div class="impact-block-content">
-    <div class="icon-space">
+        <div class="icon-space">
             <img src="<?php echo esc_url(get_template_directory_uri() . '/images/blueLine.svg'); ?>"  />
             <img src="<?php echo esc_url(get_template_directory_uri() . '/images/blueStar.svg'); ?>"  />
-        </div>
-        <h2 class="impact-block-headline"><?php echo esc_html($impact_block_headline); ?></h2>
-        <p class="impact-block-description"><?php echo esc_html($impact_block_description); ?></p>
+        </div>    <div class="impact-block-content">
+
+        <div class="impact-block-headline"><?php echo (nl2br($impact_block_headline)); ?></div>
+        <p class="impact-block-description"><?php echo (nl2br($impact_block_description)); ?></p>
         <div class="impact-block-button">
             <?php 
             get_template_part('template-parts/button', null, [
-                'button' => $impact_block_button
+                'button' => $button
+            ]); 
+            ?>
+        </div>
+    </div>
+</div>
+//desktop
+<div class="impact-block-desktop" <?php if ($impact_block_background_image && $impact_block_background_image['url']): ?>
+    style="background-image: url('<?php echo esc_url($impact_block_background_image['url']); ?>');"
+    <?php else: ?>style="background-color: $gray; height: pxToRem(50);"<?php endif; ?>>
+        <div class="icon-space">
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/images/blueLine.svg'); ?>"  />
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/images/blueStar.svg'); ?>"  />
+        </div>    <div class="impact-block-content">
+
+        <div class="impact-block-headline"><?php echo (nl2br($impact_block_headline)); ?></div>
+        <p class="impact-block-description"><?php echo (nl2br($impact_block_description)); ?></p>
+        <div class="impact-block-button">
+            <?php 
+            get_template_part('template-parts/button', null, [
+                'button' => $button
             ]); 
             ?>
         </div>
