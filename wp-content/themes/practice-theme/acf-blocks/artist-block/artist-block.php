d07bed3502158672c6b135e7876b397723c1786f
OfriG <ofri900@gmail.com>
2025-09-14 12:05:41 +0300

    almost done  dotes problem

--- wp-content/themes/practice-theme/acf-blocks/artist-block/artist-block.php
+++ wp-content/themes/practice-theme/acf-blocks/artist-block/artist-block.php
@@ -1,5 +1,6 @@
 <?php 
-$headline_image = get_field('headline_image');
+$headline_image_mobile = get_field('headline_image_mobile');
+$headline_image_desktop = get_field('headline_image_desktop');
 $description = get_field('description');
 $gallery = get_field('gallery'); 
 ?>
@@ -7,7 +8,8 @@ $gallery = get_field('gallery');
     <div class="artist-block-content">
         <div class="artist-block-text">
         <div class="artist-block-content-headline">
-            <img src="<?php echo $headline_image['url']; ?>" alt="<?php echo $headline_image['alt']; ?>">
+            <img class="headline-mobile" src="<?php echo $headline_image_mobile['url']; ?>" alt="<?php echo $headline_image_mobile['alt']; ?>">
+            <img class="headline-desktop" src="<?php echo $headline_image_desktop['url']; ?>" alt="<?php echo $headline_image_desktop['alt']; ?>">
         </div>
         <div class="artist-block-content-description">
             <p><?php echo nl2br($description); ?></p>
@@ -44,23 +46,49 @@ $gallery = get_field('gallery');
                 </div>
             <?php endif; ?>
         <!-- desktop gallery -->
-        <div class="artist-block-gallery-desktop">
-            <?php if ($gallery && count($gallery) > 0): 
-                $first_artist_item = $gallery[0]; // Get only the first item
-            ?>
+        <div class="artist-block-gallery-desktop" data-gallery-count="<?php echo count($gallery); ?>">
+            <?php if ($gallery && count($gallery) > 0): ?>
                 <div class="gallery-navigation-container">
-                    <div class="gallery-arrow gallery-arrow-left">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-left.svg" alt="Previous">
+                    <div class="gallery-arrow gallery-arrow-left" id="gallery-prev">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-right.svg" alt="Previous">
                     </div>
                     
-                    <div class="artist-gallery-item-desktop">
-                        <img src="<?php echo esc_url($first_artist_item['image']['url']); ?>" 
-                             alt="<?php echo esc_attr($first_artist_item['image']['alt'] ?? ''); ?>" 
-                             loading="lazy">
+                    <div class="desktop-gallery-content" id="gallery-container">
+                        <?php foreach ($gallery as $index => $artist_item): ?>
+                            <div class="gallery-slide" data-slide="<?php echo $index; ?>" <?php echo $index === 0 ? 'style="display: block;"' : 'style="display: none;"'; ?>>
+                                <div class="artist-details-upper">
+                                    <?php if ($artist_item['first_headline']): ?>
+                                        <h3 class="artist-name-desktop"><?php echo esc_html($artist_item['first_headline']); ?></h3>
+                                    <?php endif; ?>
+                                    
+                                    <?php if ($artist_item['second_headline']): ?>
+                                        <h4 class="artwork-title-desktop"><?php echo esc_html($artist_item['second_headline']); ?></h4>
+                                    <?php endif; ?>
+                                </div>
+                                
+                                <div class="artist-gallery-item-desktop">
+                                    <img src="<?php echo esc_url($artist_item['image']['url']); ?>" 
+                                         alt="<?php echo esc_attr($artist_item['image']['alt'] ?? ''); ?>" 
+                                         loading="lazy">
+                                </div>
+                                
+                                <div class="artist-details-lower">
+                                    <?php if ($artist_item['description']): ?>
+                                        <p class="artwork-description-desktop"><?php echo nl2br(esc_html($artist_item['description'])); ?></p>
+                                    <?php endif; ?>
+                                    
+                                    <?php if ($artist_item['email']): ?>
+                                        <a href="mailto:<?php echo esc_attr($artist_item['email']); ?>" class="artist-email-desktop">
+                                            <?php echo esc_html($artist_item['email']); ?>
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
                     
-                    <div class="gallery-arrow gallery-arrow-right">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-right.svg" alt="Next">
+                    <div class="gallery-arrow gallery-arrow-right" id="gallery-next">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-left.svg" alt="Next">
                     </div>
                 </div>
             <?php endif; ?>
@@ -71,3 +99,55 @@ $gallery = get_field('gallery');
         </div>
     </div>
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryContainer = document.getElementById('gallery-container');
+    const prevButton = document.getElementById('gallery-prev');
+    const nextButton = document.getElementById('gallery-next');
+    const galleryDesktop = document.querySelector('.artist-block-gallery-desktop');
+    
+    if (!galleryContainer || !prevButton || !nextButton || !galleryDesktop) {
+        return; 
+    }
+    
+    const slides = galleryContainer.querySelectorAll('.gallery-slide');
+    const totalSlides = slides.length;
+    let currentSlide = 0;
+    
+    if (totalSlides <= 1) {
+        prevButton.style.display = 'none';
+        nextButton.style.display = 'none';
+        return;
+    }
+    
+    function showSlide(index) {
+        slides.forEach(slide => slide.style.display = 'none');
+        
+        if (slides[index]) {
+            slides[index].style.display = 'block';
+        }
+    }
+    
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    }
+    
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+        showSlide(currentSlide);
+    }
+    
+    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', prevSlide);
+    
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 'ArrowLeft') {
+            prevSlide();
+        } else if (e.key === 'ArrowRight') {
+            nextSlide();
+        }
+    });
+});
+</script>
