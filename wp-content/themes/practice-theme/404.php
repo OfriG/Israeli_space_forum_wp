4bc81fc5bf8e96b086005ebd78dabf4519dde04a
OfriG <ofri900@gmail.com>
2025-09-16 20:40:21 +0300

    fix pr

--- wp-content/themes/practice-theme/404.php
+++ wp-content/themes/practice-theme/404.php
@@ -3,25 +3,30 @@
 get_header();
 ?>
 <?php
-$headline = get_field('headline', 'option');
 $headline_image = get_field('headline_image', 'option');
 $description = get_field('description', 'option');
 $button = get_field('button', 'option');
 $background_image = get_field('background', 'option');
 ?>
 
-<div class="error-block" <?php if ($background_image && $background_image['url']): ?>
+<div class="error-block" <?php if ($background_image && isset($background_image['url']) && $background_image['url']): ?>
     style="background-image: url('<?php echo esc_url($background_image['url']); ?>');"
     <?php endif; ?>>
     <div class="error-block-content">
+            <?php if ($headline_image && isset($headline_image['url']) && $headline_image['url']): ?>
             <div class="headline-image">
-                <img src="<?php echo esc_url($headline_image['url']); ?>" />
+                <img src="<?php echo esc_url($headline_image['url']); ?>" alt="<?php echo esc_attr($headline_image['alt'] ?? ''); ?>" />
             </div>
+            <?php endif; ?>
+            <?php if ($description): ?>
             <div class="description">
-            <p><?php echo esc_html($description); ?></p>
+                <p><?php echo esc_html($description); ?></p>
             </div>
+            <?php endif; ?>
+            <?php if ($button): ?>
             <div class="button">
-            <?php get_template_part('template-parts/button', null, ['button' => $button]); ?>
+                <?php get_template_part('template-parts/button', null, ['button' => $button]); ?>
             </div>
+            <?php endif; ?>
     </div>
 </div>
