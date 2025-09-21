750d759701459d34a85ee652ed486c654d934be0
OfriG <ofri900@gmail.com>
2025-09-21 19:05:59 +0300

    fix inner company

--- wp-content/themes/practice-theme/archive-inner_company.php
+++ wp-content/themes/practice-theme/archive-inner_company.php
@@ -1,6 +1,6 @@
 <?php
 get_header();
-
+wp_enqueue_style('innerCompany-block-css', get_template_directory_uri() . '/dist/css/innerCompany-block.css?v=' . (file_exists(get_template_directory() . '/dist/css/inner-company-block.css') ? filemtime(get_template_directory() . '/dist/css/innerCompany-block.css') : '1.0'));
 if (have_posts()) :
     while (have_posts()) : the_post();
         ?>
@@ -8,6 +8,8 @@ if (have_posts()) :
             <div class="entry-content">
                 <?php
                 the_content();
+                include get_template_directory() . '/acf-blocks/innerCompany-block/innerCompany-block.php';
+
                 ?>
             </div>
         </article>
