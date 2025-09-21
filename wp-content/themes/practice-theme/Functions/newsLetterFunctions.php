a58f80cb9e49ed3aea01d8b1166d63ccf656c54d
OfriG <ofri900@gmail.com>
2025-09-21 10:58:06 +0300

    newsletter email check

--- wp-content/themes/practice-theme/Functions/newsLetterFunctions.php
+++ wp-content/themes/practice-theme/Functions/newsLetterFunctions.php
@@ -1,15 +1,5 @@
 <?php
 
-function enqueue_newsletter_scripts()
-{
-    wp_enqueue_script('newsletter-js', get_theme_file_uri('/dist/js/newsLetter.js'), ['jquery'], '1.0', true);
-
-    wp_localize_script('newsletter-js', 'newsletterSettings', [
-        'ajaxUrl' => admin_url('admin-ajax.php'),
-        'nonce' => wp_create_nonce('newsletter_nonce')
-    ]);
-}
-add_action('wp_enqueue_scripts', 'enqueue_newsletter_scripts');
 
 // take care of the request
 add_action('wp_ajax_practice_newsletter', 'handle_newsletter_signup');
